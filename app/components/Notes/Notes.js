b940538324db740937521142661357e85d320685
Richard Mable <richardbmable@gmail.com>
2016-05-05 15:33:32 -0400

    added validations via PropTypes on Notes, Bio, username

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,14 +1,19 @@
 var React = require('react');
+var NotesList = require('./NotesList')
 
 var Notes = React.createClass({
+	propTypes: {
+		username: React.PropTypes.string.isRequired,
+		notes: React.PropTypes.array.isRequired
+	},
 	render: function(){
+		console.log('Notes: ', this.props.notes)
 		return (
 			<div> 
-			<p>Notes!</p> 
-			<p>{this.props.notes}</p>
+			<h3>Notes for {this.props.username}</h3>
+			<NotesList notes={this.props.notes} />
 			</div>
 		)
-
 	}
 });
 
