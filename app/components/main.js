8c2820fb5a343a04396762b8930d2aa6516c1641
Richard Mable <richardbmable@gmail.com>
2016-05-05 11:01:01 -0400

    added routing and more components

--- app/components/main.js
+++ app/components/main.js
@@ -1,17 +1,24 @@
 var React = require('react');
 
-var ReactDOM = require('react-dom');
-
 // createClass creates a React component
 var Main = React.createClass({
 	render: function(){
-		return: (
-			<div>Hello World!</div>
+		return (
+			<div className="main-container">
+				<nav className="navbar navbar-default" role="navigation">
+					<div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+						MENU
+					</div>
+				</nav>
+				<div className="container">
+					{this.props.children}
+				</div>
+			</div>
 		)
 
 	}
 })
 
 
-// grabs the element div with id app and renders the main compoent to it
-ReactDOM.render(<Main />, document.getElementById('app'))
+// whenever we require Main, we are going to get the component above
+module.exports = Main;
